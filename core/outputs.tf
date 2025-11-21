ba6514a4366377656c2de9c3c8c54630aedcdfeb
unfettered-one <pakhaleyash7@gmail.com>
2025-11-22 03:02:57 +0530

    fix: fix outputs .tf

--- core/outputs.tf
+++ core/outputs.tf
@@ -1,28 +1,27 @@
 output "lambda_function_name" {
   value = (var.use_zip ?
-    aws_lambda_function.zip_lambda[0].function_name :
-  aws_lambda_function.container_lambda[0].function_name)
+    try(aws_lambda_function.zip_lambda[0].function_name, null) :
+  try(aws_lambda_function.container_lambda[0].function_name, null))
 }
 
 output "lambda_function_arn" {
   value = (var.use_zip ?
-    aws_lambda_function.zip_lambda[0].arn :
-  aws_lambda_function.container_lambda[0].arn)
+    try(aws_lambda_function.zip_lambda[0].arn, null) :
+  try(aws_lambda_function.container_lambda[0].arn, null))
 }
 
 output "lambda_invoke_arn" {
   value = (var.use_zip ?
-    aws_lambda_function.zip_lambda[0].invoke_arn :
-  aws_lambda_function.container_lambda[0].invoke_arn)
+    try(aws_lambda_function.zip_lambda[0].invoke_arn, null) :
+  try(aws_lambda_function.container_lambda[0].invoke_arn, null))
 }
 
 output "lambda_image_repository_url" {
-  value = (var.use_container && var.use_ecr ?
-    aws_ecr_repository.lambda_repo[0].repository_url :
+  value = ((var.use_container && var.use_ecr) ?
+    try(aws_ecr_repository.lambda_repo[0].repository_url, null) :
   null)
 }
 
-# OUTPUT: Role ARN for Lambda
 output "lambda_role_arn" {
   value = aws_iam_role.lambda_exec_role.arn
 }

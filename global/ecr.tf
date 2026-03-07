9f652d66c541c662cf4e977f0bbf05d6adb3c2b6
unfettered-one <pakhaleyash7@gmail.com>
2026-03-07 12:44:25 +0530

    fix: update ECR lifecycle policy to expire untagged images after 1 day

--- global/ecr.tf
+++ global/ecr.tf
@@ -21,11 +21,12 @@ resource "aws_ecr_lifecycle_policy" "lambda_images_lifecycle" {
     rules = [
       {
         rulePriority = 1
-        description  = "Keep last 10 images"
+        description  = "Expire untagged images after 1 day"
         selection = {
-          tagStatus   = "any"
-          countType   = "imageCountMoreThan"
-          countNumber = 10
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = 1
         }
         action = {
           type = "expire"
